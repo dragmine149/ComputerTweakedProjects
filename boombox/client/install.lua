144e4c8b2e0831e8ea04b501e23383f1d0943680
dragmine <dan@thebanners.uk>
2025-05-18 20:37:36 +0100

    Improved how files are downloaded on setup

--- boombox/client/install.lua
+++ boombox/client/install.lua
@@ -1,12 +1,28 @@
+local function downloadFile(filename, url)
+    local res = http.get(url)
+    if not res then error("Error downloading file from " .. url) end
+    local data = res.readAll()
+    res.close()
+    local file = fs.open(filename, "w")
+    if not file then error("Can't write to file " .. filename .. "...") end
+    file.write(data)
+    file.close()
+end
+
 print("Downloading basalt (UI Manager)")
 print()
 if fs.exists("/Boombox/basalt.lua") then fs.delete("/Boombox/basalt.lua") end
-shell.run("wget", "run", "https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua", "-r")
-fs.move("/basalt.lua", "/Boombox/basalt.lua")
+shell.run("wget", "run", "https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua", "-r", "/Boombox/basalt.lua")
 
 print("Downloading AUKit")
 print()
 if fs.exists("/Boombox/AUKit.lua") then fs.delete("/Boombox/AUKit.lua") end
-shell.run("wget", "https://github.com/MCJack123/AUKit/releases/download/1.9.1/aukit.min.lua", "/Boombox/AUKit.lua")
+downloadFile("/Boombox/AUKit.lua", "https://github.com/MCJack123/AUKit/releases/download/1.9.1/aukit.min.lua")
+
+print("Downloading BetterBetterBlittle")
+print()
+if fs.exists("/Boombox/utils/betterbetterblittle.lua") then fs.delete("/Boombox/utils/betterbetterblittle.lua") end
+downloadFile("/Boombox/utils/betterbetterblittle.lua",
+    "https://raw.githubusercontent.com/Xella37/Pine3D/bf70ce8dfd293c5cfc795183cf1ba8a6d230be73/betterblittle.lua")
 
 print("Downloading Boombox...")
