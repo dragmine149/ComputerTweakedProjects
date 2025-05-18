3af0494b42177f32bc96a389625e5cef41ddcce9
dragmine <dan@thebanners.uk>
2025-05-18 20:39:07 +0100

    Work on downloading stuff

--- boombox/client/network/file.lua
+++ boombox/client/network/file.lua
@@ -1,54 +1,101 @@
 local args = {...}
 local switch = require("/Boombox.utils.switch")
+local log = require("/Boombox.utils.log")
 
 local file = {}
 
+---Request a server with retry support.
+---@param url string The server to ping.
+---@param headers any
+---@param binary any
+---@return string|nil
 local function request(url, headers, binary)
-    local response
-
-    if file.__autoRetry.enabled then
-        file.__autoRetry[url] = file.__autoRetry[url] or 2
+    local valid, reason = http.checkURL(url)
+    if not valid then
+        log.error("Invalid url: " .. reason)
+        os.queueEvent("boombox_download_error", reason)
+        return
     end
+    log.info("Requesting " .. url)
+    local response, message, failed
 
-    while not response and file.__autoRetry[url] < 3 do
-        print("Requesting: " .. url)
-        response = http.get(url, headers, binary)
+    file.retry = 2
+    if file.retryEnabled then file.retry = 0 end
+
+    log.info(response)
+    log.info("retry: " .. tostring(file.retry))
+    while not response and file.retry < 3 do
+        log.debug("Requesting: " .. url)
+        log.info(response)
+        log.info("retry: " .. tostring(file.retry) .. ", enabled: " .. tostring(file.retryEnabled))
+
+        response, message, failed = http.get(url, headers, binary)
         if response then
             local data = response.readAll()
             response.close()
+            log.info('Received: '.. tostring(data))
             return data
         end
 
-        file.__autoRetry[url] = file.__autoRetry[url] + 1
+        if message then
+            log.error(message)
+        end
+
+        if failed then
+            local failedData = failed.readAll()
+            failed.close()
+            log.info('Failed: ' .. tostring(failedData))
+        end
+
+        file.retry = file.retry + 1
+        os.queueEvent("boombox_retry", file.retry)
     end
+    log.info("Completed while loop.")
+
+    -- return response
 end
 
 function file.new()
     file.__path = ""
     file.__data = {}
     file.__info = {}
-    file.__autoRetry = {
-        enabled = false
-    }
-    file.__yt = file.__path:match("http[s]?://[w]?[w]?[w]?%.?youtube%.com/watch%?v=") or file.__path:match("http[s]?://youtu%.be/")
+    file.retry = 0
+    file.retryEnabled = false
 
     file:listen()
 end
 
 function file:getInfo()
     if self.__yt then
-       local response = request("https://ytdlp.online/stream?command=--get-title --skip-download " .. self.__path)
-       if not response then
-           os.queueEvent("boombox_download_error", self.__path)
-           return
-       end
-       file.__info.title = response:match("\n.-\n(data: (.+))")
+        log.info("retry: " .. tostring(file.retry) .. ", enabled: " .. tostring(file.retryEnabled))
+
+        local response = request("https://ytdlp.online/stream?command=--get-title%20--skip-download%20--xff%20us%20" .. self.__path)
+        log.info(response)
+        if not response then
+            log.error("Error in response stream!")
+            os.queueEvent("boombox_download_error", self.__path)
+            return
+        end
+        file.__info.title = response:match("\n.-\n(data: (.+))")local title = nil
+        for line in response:gmatch("[^\r\n]+") do
+            if line:match("^data:") then
+                if title then
+                    title = line:match("^data:%s*(.-)$")
+                    break
+                else
+                    title = true
+                end
+            end
+        end
+        file.__info.title = title
+        os.queueEvent("boombox_download_info", file.__info.title)
+        return
     end
     -- assume title is in the file name.
     local file_name = self.__path:match("([^/]+)%.");
     file.__info.title = file_name
 
-    os.queueEvent("boombox_download_info", file.__info)
+    os.queueEvent("boombox_download_info", file.__info.title)
 end
 
 function file:download()
@@ -67,13 +114,14 @@ function file:download()
 end
 
 function file:listen()
-    local data = {os.pullEvent("boombox_request")}
     local ListenSwitch = switch()
-        :case("file", function()
+        :case("file", function(data)
             self.__path = data[2]
+            self.__yt = self.__path:match("http[s]?://[w]?[w]?[w]?%.?youtube%.com/watch%?v=") or self.__path:match("http[s]?://youtu%.be/")
         end)
-        :case("auto", function()
-            self.__autoRetry.enabled = data[2]
+        :case("retry", function(data)
+            -- log.info(textutils.serialise(data))
+            self.retryEnabled = data[2]
         end)
         :case("info", function()
             self:getInfo()
@@ -81,11 +129,26 @@ function file:listen()
         :case("download", function()
             self:download()
         end)
+        :case("terminate", function()
+            log.warn("Preventing termination")
+        end)
+        :case("http_check", function (url, success, reason)
+            if not success then
+                log.error(url .. " provided " .. reason)
+            end
+        end)
         :default(function()
-            os.queueEvent("boombox_download_error", "unknown event")
+            -- os.queueEvent("boombox_download_error", "unknown event")
         end)
 
-    ListenSwitch(data[1])
+    while true do
+        local data = {os.pullEventRaw("boombox_request")}
+        table.remove(data, 1)
+        log.info('Received data')
+        log.info(textutils.serialise(data))
+
+        ListenSwitch(data[1], data)
+    end
 end
 
 
--- boombox/client/ui/downloader.lua
+++ boombox/client/ui/downloader.lua
@@ -37,38 +37,64 @@ Please don't worry. The music will continue playing even with the computer await
             shell.switchTab(self.tab)
         end)
         :listenEvent("terminal_input", true)
+        :listenEvent("boombox_download_error", true)
+        :listenEvent("boombox_download_info", true)
 
     downloader.info = screen:addLabel("ui_downloader_info")
         :setSize("{parent.width}", 3)
-        :setPosition(0, downloader.input.height + downloader.description.height + downloader.title.height)
+        :setPosition(1, "{ui_downloader_input.y + ui_downloader_input.height + 1}")
         :setText("")
 
 
     function downloader.input:handleEvent(event, data)
-        -- downloader.log.info("EventReceived: " .. event .. " Data: " .. data)
+        -- downloader.log.info("EventReceived: " .. event .. " Data: " .. pretty.pretty(data))
         if event == "terminal_input" then
             downloader.input:setText(data)
-            os.queueEvent("boombox_request", "file")
+            os.queueEvent("boombox_request", "file", data)
             os.queueEvent("boombox_request", "info")
+            self.tab = 1 -- switch back to our tab.
+            shell.switchTab(self.tab)
         end
 
         if event == "boombox_download_error" or event == "boombox_download_info" then
             downloader.info:setText(data)
+
+            if not string.find(data, "youtube:") then
+                downloader.button:setColor("{self.clicked and colors.white or colors.black}", "{self.clicked and colors.gray or colors.lightGray}")
+            end
         end
     end
 
     downloader.button = screen:addButton("ui_downloader_button")
         :setText("Download")
         :setSize("{parent.width}", 1)
-        :setPosition(0, downloader.input.height + downloader.description.height + downloader.title.height + downloader.info.height)
+        :setPosition(1, "{ui_downloader_info.y + ui_downloader_info.height + 1}")
+        :setColor("{colors.gray}", "{colors.black}")
 
     downloader.retry = screen:addCheckbox("ui_downloader_retry")
-        :setText("Auto retry?: ")
+        :setText("Auto retry?: [ ] (0/1)")
+        :setCheckedText("Auto retry?: [x] (0/3)")
         :setSize("{parent.width}", 1)
-        :setPosition(0, downloader.input.height + downloader.description.height + downloader.title.height + downloader.info.height + downloader.button.height)
+        :setPosition(1, "{ui_downloader_button.y + ui_downloader_button.height + 1}")
         :onClick(function()
-            os.queueEvent("boombox_request", "retry", downloader.retry.getChecked())
+            os.queueEvent("boombox_request", "retry", not downloader.retry:getChecked())
         end)
+        :listenEvent("boombox_retry")
+        :listenEvent("boombox_request")
+
+    function downloader.retry:handleEvent(event, data)
+        if event == "boombox_retry" then
+            downloader.retry:setText("Auto retry?: [ ] (" .. data .. "/1)")
+            downloader.retry:setCheckedText("Auto retry?: [x] (" .. data .. "/3)")
+        end
+        if event == "boombox_request" then
+            if data == "info" then
+                downloader.retry
+                    :setText("Auto retry?: [ ] (0/1)")
+                    :setCheckedText("Auto retry?: [x] (0/3)")
+            end
+        end
+    end
 
     downloader.network = multishell.launch(shellAdditions.createShellEnv("rom/programs"), "rom/programs/shell.lua", "Boombox/network/file.lua")
     multishell.setTitle(downloader.network, "Boombox Downloader")
